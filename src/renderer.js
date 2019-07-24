842c49d6bb338a36b6f0b0c2ed8106f4239eaf2f
Nihal Shriwastawa <NihalSh@users.noreply.github.com>
2019-07-24 20:41:35 +0530

    chore: setup react and jsx transpiler

--- src/renderer.js
+++ src/renderer.js
@@ -25,7 +25,14 @@
  *  });
  * ```
  */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
+
+ReactDOM.render(
+  <span>this works!</span>,
+  document.getElementById('app'),
+);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,9 +1,21 @@
+const path = require('path');
 const rules = require('./webpack.rules');
+
+const srcPath = path.join(__dirname, 'src');
+
+rules.push({
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  include: srcPath,
+  loader: 'babel-loader',
+});
 
 rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
+
+
 
 module.exports = {
   // Put your normal webpack config below here
